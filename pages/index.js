66b87a6cffa8338b57a79cf6295f7c3743387107
Eduardo Rico <erico@intelimetrica.com>
2022-01-23 20:23:12 -0600

    cambio en el botón

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import { useWeb3React } from '@web3-react/core'
-import { useEffect } from 'react'
+import { useCallback, useEffect } from 'react'
+import { connector } from '../config/web3'
 import styles from '../styles/Home.module.css'
 
 
@@ -9,23 +10,46 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   const {activate, active, deactivate, account, error, chainId} = useWeb3React()
 
+  const connect = useCallback(() =>{
+    activate(connector)
+    localStorage.setItem('previuslyConnected', true)
+  }, [activate])
+
   useEffect(() => {
-    
-  },[])
+    if(localStorage.getItem('previuslyConnected') === 'true'){
+      connect()
+    }
+  }, [connect])
 
-  const connect = () =>{
 
-  }
 
   const disconnect = () =>{
-    
+    deactivate()
+    localStorage.removeItem('previuslyConnected')
+  }
+
+  if (error){
+    return <p> Ha habido un error (desde el hook de web3) </p>
   }
 
 
   return (
     <div className={styles.container}>
       <h1>Test de Web3.0</h1>
-      <button onClick={connect}>Conectar a la wallet</button>
+      {
+        active 
+        ? <>
+            <p>
+              Estás conectado a la red {chainId}
+            </p>
+            <p>
+              Tu dirección es {account}
+            </p>
+            <button onClick={disconnect}>Desconectar a la wallet</button>
+          </>
+        : <button onClick={connect}>Conectar a la wallet</button>
+      }
+      
     </div>
   )
 }
